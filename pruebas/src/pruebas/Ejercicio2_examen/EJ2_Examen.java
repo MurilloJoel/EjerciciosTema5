fd9b16001ae968a6f503d3ccddeec2248e8c9deb
developer <developer@cec4bdae6b9e>
2023-12-11 12:39:08 +0000

    Examen primera evaluacion programacion

---
+++ pruebas/src/pruebas/Ejercicio2_examen/EJ2_Examen.java
@@ -0,0 +1,52 @@
+/*
+
+ Escribe un programa que utilice una clase Empresa que tenga como atributos el nombre, el número de empleados y los beneficios (con decimales y podrían ser negativos). Habrá un constructor que recibirá 3 argumentos correspondientes a los 3 atributos.
+
+Se utilizará la encapsulación. El método setNumEmpleados no permitirá que el nº de empleados sea menor o igual a cero.
+
+Tendrá un método toString que devolverá el String de su nombre, su nº de empleados y sus beneficios separados por tabuladores.
+
+        Ejemplo:
+
+        Market Digital      28      12522.35
+
+    El programa tendrá también una clase HistoriaEmpresas:
+
+    Primero se escribirá por pantalla la fecha de hoy haciendo uso de la clase que Java tiene en sus librerías de manejo tiempos históricos.
+
+    Se leerá un número entero n por teclado que será el número de empresas que se van a crear. 
+    Con ese número se crearán n objetos Empresa, donde el nombre se leerá por teclado,
+    el número de empleados será un número al azar entre 1 y 50, 
+    los beneficios serán un número que se leerá por teclado (se capturará la excepción en caso de que no se escriba un número, 
+    escribiendo el mensaje "No es un número" y terminando la ejecución del programa).
+
+        A continuación, se mostrará un menú con las siguientes opciones:
+
+            1. Mostrar empresas (todas)
+
+            2. Mostrar empresa/s con más beneficios
+
+            3. Mostrar el número medio de empleados de las empresas introducidas.
+
+            0. Salir
+
+
+
+
+ */
+package pruebas.Ejercicio2_examen;
+
+/**
+ *
+ * @author developer
+ */
+public class EJ2_Examen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ pruebas/src/pruebas/Pruebas.java
@@ -0,0 +1,46 @@
+/*
+                                 EJERCICIO 1 DEL EXAMEN
+
+Las nuevas políticas de seguridad de contraseñas que se aplican en el instituto recomiendan que éstas sean de al menos 8 caracteres,
+que contengan al menos una letra mayúscula, al menos 3 minúsculas, al menos 2 dígitos que no se repitan en toda la contraseña,
+uno o más de los siguientes caracteres no alfanuméricos: #, %, +, *, -, _, $ y que empiece por letra. 
+No se permitirá ningún otro carácter que no esté contemplado entre los antedichos.
+
+Realiza en Java un programa que lea por teclado una contraseña y diga si es válida o no. Si no es válida,
+que diga por pantalla el/los motivos.
+
+
+Ejemplos:
+
+        Ca51ta_7
+
+    Es válida
+
+
+        C0raz0n
+
+    No es válida
+
+    No tiene la longitud mínima
+
+    Tiene dos dígitos repetidos
+
+    No tiene al menos un caracter no alfanumérico
+
+ */
+package pruebas;
+
+/**
+ *
+ * @author developer
+ */
+public class Pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
